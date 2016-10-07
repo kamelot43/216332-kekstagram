ca50277b7e4fbf08410ec483a7334a2e3bb04bd7
Andrey Pudov <andrepud@mail.ru>
2016-10-07 18:23:13 +0300

    Изменение  имен переменных

--- src/js/check.js
+++ src/js/check.js
@@ -1,31 +1,31 @@
 
 
-var getMessage = function (theFirstVariable, theSecondVariable) {
+var getMessage = function (a, b) {
 //Условие проверки первой переменной(булевное значение или нет?):	
-if (typeof theFirstVariable === 'boolean') {
-console.info('Переданное GIF-изображение анимировано и содержит' + theSecondVariable + 'кадров');
+if (typeof a === 'boolean') {
+console.info('Переданное GIF-изображение анимировано и содержит' + b + 'кадров');
 } else {
 console.info('Переданное GIF-изображение не анимировано');
 }
 //Условие проверки первой переменной (числовое значение или нет?): 
-if (typeof theFirstVariable === 'number') {
-var multiplicationOfVariable = theSecondVariable * 4;
-console.info('Переданное SVG-изображение содержит' + theFirstVariable + 'объектов и'+ multiplicationOfVariable + 'атрибутов' );
+if (typeof a === 'number') {
+var multiplicationOfVariable = b * 4;
+console.info('Переданное SVG-изображение содержит' + a + 'объектов и'+ multiplicationOfVariable + 'атрибутов' );
 }
 //Условие проверки первой переменной(массив или нет?):
-if(Array.isArray(theFirstVariable) && !Array.isArray(theSecondVariable)) {
+if(Array.isArray(a) && !Array.isArray(b)) {
 sum = 0;
-for (var i = 0; i < theFirstVariable.length; i++) {
-sum = sum + theFirstVariable[i];
+for (var i = 0; i < a.length; i++) {
+sum = sum + a[i];
 var amountOfRedPoints=sum;
 }
 console.info('Количество красных точек во всех строчках изображения:' + amountOfRedPoints);
 }
 //Условие проверки двух переменных (a и b массивы?):
-if(Array.isArray(theFirstVariable) && Array.isArray(theSecondVariable)) {
+if(Array.isArray(a) && Array.isArray(b)) {
 sum = 0;
-for (var i = 0; i < theFirstVariable.length && theSecondVariable.length; i++) {
-sum = sum + (theFirstVariable[i]*theSecondVariable[i]);
+for (var i = 0; i < a.length && b.length; i++) {
+sum = sum + (a[i]*b[i]);
 var artifactsSquare = sum;
 }
 console.info('Общая площадь артефактов сжатия:' + artifactsSquare + 'пикселей');
