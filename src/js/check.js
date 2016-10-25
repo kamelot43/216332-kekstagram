d70a822ea2cf432572ee30bd45b3ebec5493432c
Andrey Pudov <andrepud@mail.ru>
2016-10-25 23:09:09 +0300

    Бесуспешные попытки (начинаем програмировать)

--- src/js/check.js
+++ src/js/check.js
@@ -1,29 +1,28 @@
-'use strict';
-
- var getMessage = function (a, b) {
-
-   if (typeof a == 'boolean') {
-     if (a) {
-       return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
-     } else {
-       return "Переданное GIF-изображение не анимировано";
-     }
-   }
-   if (typeof a == 'number') {
-     return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
-   }
-   if (Array.isArray(a) && Array.isArray(b)) {
-     var artifactsSquare = 0;
-     for (var i = 0; i < a.length; i++) {
-       artifactsSquare += a[i] * b[i];
-     }
-     return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
-   } else if (Array.isArray(a)){
-     var amountOfRedPoints = 0;
-     for (var i = 0; i < a.length; i++) {
-       amountOfRedPoints = amountOfRedPoints + a[i];
-     }
-     return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
-   }
-   return "Переданы некорректные данные";
- }
+var getMessage = function(a, b) {
+  var result = '';
+  if (typeof a === 'boolean') {
+    if (a === true) {
+      result = 'Переданное GIF-изображение анимировано и содержит' + b + 'кадров';
+    } else {
+      result = 'Переданное GIF-изображение не анимировано';
+    }
+  } else if (typeof a === 'number') {
+    var multiplicationOfVariable = b * 4;
+    result = 'Переданное SVG-изображение содержит' + a + 'объектов и' + multiplicationOfVariable + 'атрибутов';
+  } else if (Array.isArray(a) && !Array.isArray(b)) {
+    var amountOfRedPoints = 0;
+    for (var i = 0; i < a.length; i++) {
+      amountOfRedPoints += a[i];
+      result = 'Количество красных точек во всех строчках изображения:' + amountOfRedPoints;
+    }
+  }else if (Array.isArray(a) && Array.isArray(b)) {
+    var artifactsSquare = 0;
+    for (var i = 0; i < a.length && b.length; i++) {
+      artifactsSquare += a[i] * b[i];
+      result = 'Общая площадь артефактов сжатия:' + artifactsSquare + 'пикселей';
+    }
+  } else {
+    result = 'Переданы некорректные данные';
+  }
+  return result;
+}
