386e0978a9ed731581aafb4b9257d177a0371cdb
Andrey Pudov <andrepud@mail.ru>
2016-10-28 14:33:44 +0300

    DATA_BASE_URL возвращено на свое место.
    Travis ругается. Я вернул DATA_BASE_URL в файл load.js . Теперь ругаться не должен. Хотя файл у меня находится в двух местах.

--- src/js/load.js
+++ src/js/load.js
@@ -1,7 +1,8 @@
 'use strict';
 var connect = function(url, callback) {
   //Набор переменных для работы с модулем load.js
-  var callbackName = '__jsonpCallback' + Date.now(); 
+  var callbackName = '__jsonpCallback' + Date.now();
+  var DATA_BASE_URL = 'http://localhost:1507/api/pictures';
   window[callbackName] = function(data) {
     callback(data);
   };
