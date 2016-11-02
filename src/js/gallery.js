f5a526b4d7b8a9e819f4c80ac5429ae122640e1a
Andrey Pudov <andrepud@mail.ru>
2016-11-02 22:28:48 +0300

    Создание файла галерея

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,11 +1,55 @@
 'use strict';
+//////////////////////////////////////////////////////////////////////
+//ФУНКЦИЯ-КОНСТРУКТОР GALLERY И ЕЕ ОБЪЕКТ ПРОТОТИП
 var Gallery = function() {
   this.activePicture = 0;
   this.elementPhoto = document.querySelector('.gallery-overlay');
   this.closeGallery = document.querySelector('.gallery-overlay-close');
   this.overlayImage = document.querySelector('.gallery-overlay-image');
+  this.galleryLikes = document.querySelector('.likes-count');
+  this.galleryComments = document.querySelector('.comments-count');
 };
-
+/////////////////////////////////////////////////////////////////////
+//ДОБАВЛЕНИЕ МЕТОДА setPictures
 Gallery.prototype.setPictures = function(pictures) {
   this.pictures = pictures;
 };
+
+////////////////////////////////////////////////////////////////////
+//ДОБАВЛЕНИЕ МЕТОДА show
+Gallery.prototype.show = function(number) {
+  this.elementPhoto.classList.remove('invisible');// Показывает фотогалерею, убирая у ее DOM-элемента класс invisible.
+//Обработчики событий (2 шт.):
+  this.closeGallery.onclick = function() {
+    var that = this;
+    that.hide();
+  };
+  this.elementPhoto.onclick = function() {
+    var that = this;
+    if (that.activePicture === that.pictures.length - 1) {
+      that.setActivePicture(0);
+    }else{
+      that.setActivePicture(number++);
+    }
+  };
+  this.setActivePicture(number);// Вызывает метод setActivePicture, передав в него параметром число, которое было передано параметром в show;
+
+
+////////////////////////////////////////////////////////////////////
+//ДОБАВЛЕНИЕ МЕТОДА hide :
+  Gallery.prototype.hide = function() {
+    this.elementPhoto.classList.add('invisible'); // Добавлет DOM-элементу фотогалереи класс invisible
+    this.elementPhoto.onclick = null;
+    this.closeGallery.onclick = null;
+  };
+////////////////////////////////////////////////////////////////////
+//ДОБАВЛЕНИЕ МЕТОДА setActivePicture :
+  Gallery.prototype.setActivePicture = function(number) {
+    this.activePicture = number;
+    this.overlayImage.src = this.pictures[this.activePicture].url;
+    this.galleryLikes = this.pictures[this.activePicture].likes;
+    this.galleryComments = this.pictures[this.activePicture].comments;
+  };
+};
+///////////////////////////////////////////////////////////////////////
+module.exports = new Gallery();
