197b4d42475a73bc897c9167e1c0916118c01ab6
Andrey Pudov <andrepud@mail.ru>
2016-11-09 00:47:49 +0300

    Модификация функции connect

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -5,6 +5,7 @@ var gallery = require('./gallery');
 var showPictures = (function() {
   var container = document.querySelector('.pictures');
   var filter = document.querySelector('.filters');
+  var PAGE_SIZE = 12; //Размер страницы
   filter.classList.add('hidden');
   var DATA_BASE_URL = 'http://localhost:1507/api/pictures';
   var renderPictures = function(pictures) {
@@ -15,6 +16,14 @@ var showPictures = (function() {
     filter.classList.remove('hidden');
     gallery.setPictures(pictures);
   };
-  connect(DATA_BASE_URL, renderPictures);
+  //Создание обновленного метода connect,который принимет на вход параметры:
+  var loadHotels = function(hotels, page) { //page -номер страницы;
+    connect(DATA_BASE_URL, { //параметр 1 - адрес;
+      from: page * PAGE_SIZE, //параметр 2 - объект со свойствами from и to, в которых записаны индексы первой и последней фотографии блока;
+      to: page * PAGE_SIZE + PAGE_SIZE,
+      filter: filter
+    }, renderPictures); //параметр 3 -функция отрисовки фотографий;
+  };
+
 })();
 module.exports = showPictures;
