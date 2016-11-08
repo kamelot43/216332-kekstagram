b34acb9b8480f8ff72c4a6252915762608e5926f
Andrey Pudov <andrepud@mail.ru>
2016-11-09 01:44:44 +0300

    Добавление обработчика событий на фильтры.

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -6,6 +6,7 @@ var showPictures = (function() {
   var container = document.querySelector('.pictures');
   var filter = document.querySelector('.filters');
   var PAGE_SIZE = 12; //Размер страницы
+  var pageNumber = 0;
   filter.classList.add('hidden');
   var DATA_BASE_URL = 'http://localhost:1507/api/pictures';
   var renderPictures = function(pictures) {
@@ -21,9 +22,20 @@ var showPictures = (function() {
     connect(DATA_BASE_URL, { //параметр 1 - адрес;
       from: page * PAGE_SIZE, //параметр 2 - объект со свойствами from и to, в которых записаны индексы первой и последней фотографии блока;
       to: page * PAGE_SIZE + PAGE_SIZE,
-      filter: filter
+      filter: hotels
     }, renderPictures); //параметр 3 -функция отрисовки фотографий;
   };
-
+  //Делигирование
+  var changeFilters = function(filterID) {
+    container.innerHTML = ''; // Очиcтка HTML
+    pageNumber = 0;
+    loadHotels(filterID, pageNumber);
+  };
+  //Добавлен е обработчика событий(клика)на фильтры
+  filter.addEventListener('click', function(evt) {
+    if(evt.target.classList.contains('filters-radio')) { //Проверка условия
+      changeFilters(evt.target.id); //Передача параметром в функцию свойства id
+    }
+  });
 })();
 module.exports = showPictures;
