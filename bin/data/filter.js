b55a3e4a5e0cd7cd1ee11c945abdba2b410bf005
Andrey Pudov <andrepud@mail.ru>
2016-11-10 00:22:53 +0300

    Внесение изменений в файл filter.js

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = function(list, filterID) {
-  return list;
-};
+  switch(filterID) {
+     case 'filter-popular':
+       return list.sort(
+         function(a, b) {
+         return b.likes - a.likes;
+       });
+       case 'filter-popular':
+         return list.sort(
+           function(a, b) {
+           return b.comments - a.comments;
+         });
+         case 'filter-new':
+           return list.sort(
+             function(a, b) {
+             return b.created - a.created; //Откуда берется поле created ?
+           });
+   }
+   return list;
+ };
